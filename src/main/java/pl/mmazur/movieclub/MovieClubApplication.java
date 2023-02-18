58c180ba8f98ca04dc78d118802d14e1e98ff9cd
Michał Mazur <michumazur89@gmail.com>
2023-02-18 22:45:05 +0100

    initial commit

---
+++ src/main/java/pl/mmazur/movieclub/MovieClubApplication.java
@@ -0,0 +1,13 @@
+package pl.mmazur.movieclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieClubApplication.class, args);
+    }
+
+}
