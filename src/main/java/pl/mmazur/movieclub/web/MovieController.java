562c56344d9a41d07d8a2a4c0e9af8712f0a2cd6
Michał Mazur <michumazur89@gmail.com>
2023-03-06 20:48:01 +0100

    implemented a 404 error page

--- src/main/java/pl/mmazur/movieclub/web/MovieController.java
+++ src/main/java/pl/mmazur/movieclub/web/MovieController.java
@@ -1,9 +1,11 @@
 package pl.mmazur.movieclub.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import pl.mmazur.movieclub.domain.movie.MovieService;
 import pl.mmazur.movieclub.domain.movie.dto.MovieDto;
 
@@ -19,8 +21,9 @@ public class MovieController {
 
     @GetMapping("/film/{id}")
     public String getMovie(@PathVariable long id, Model model) {
-        Optional<MovieDto> optionalMovie = movieService.findMovieById(id);
-        optionalMovie.ifPresent(movie -> model.addAttribute("movie", movie));
+        MovieDto movie = movieService.findMovieById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("movie", movie);
         return "movie";
     }
 }
