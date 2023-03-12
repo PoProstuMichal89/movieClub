3f5668dc0b05e06e4acff27cd216f7a2b6a6938a
Michał Mazur <michumazur89@gmail.com>
2023-03-12 21:25:51 +0100

    Added logic for creating a new account (registration process).

--- src/main/java/pl/mmazur/movieclub/config/security/CustomSecurityConfig.java
+++ src/main/java/pl/mmazur/movieclub/config/security/CustomSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -37,5 +39,10 @@ public class CustomSecurityConfig {
                 "/scripts/**",
                 "/styles/**"
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
---
+++ src/main/java/pl/mmazur/movieclub/domain/user/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package pl.mmazur.movieclub.domain.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
+    Optional<UserRole> findByName(String name);
+}
