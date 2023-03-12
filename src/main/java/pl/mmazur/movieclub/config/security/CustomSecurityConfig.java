6252ee71e410bf1b650e9444ddf04c702a87048a
Michał Mazur <michumazur89@gmail.com>
2023-03-12 21:34:33 +0100

    o fix access to the H2 console

--- src/main/java/pl/mmazur/movieclub/config/security/CustomSecurityConfig.java
+++ src/main/java/pl/mmazur/movieclub/config/security/CustomSecurityConfig.java
@@ -29,6 +29,8 @@ public class CustomSecurityConfig {
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
                         .logoutSuccessUrl("/login?logout").permitAll()
                 );
+        http.csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+        http.headers().frameOptions().sameOrigin();
         return http.build();
     }
 
