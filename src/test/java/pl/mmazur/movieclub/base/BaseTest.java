00ab5f4e9d4319296c9a4acd3ac3a94ca6c352ea
Michał Mazur <michumazur89@gmail.com>
2023-09-25 11:30:54 +0200

    added base test class and package

---
+++ src/test/java/pl/mmazur/movieclub/base/BaseTest.java
@@ -0,0 +1,26 @@
+package pl.mmazur.movieclub.base;
+
+import com.microsoft.playwright.Page;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import pl.mmazur.movieclub.factory.PlaywrightFactory;
+
+public class BaseTest {
+    protected Page page;
+    protected HomePage hp;
+    protected SearchPage search;
+    PlaywrightFactory play;
+    @BeforeClass
+    @Parameters({ "appURL", "browserType" })
+    public void setUp(String appURL, String browserType) {
+        play = new PlaywrightFactory();
+        page = play.getPage(appURL, browserType);
+        hp = new HomePage(page);
+    }
+    @AfterClass
+    public void tearDown() {
+        page.context().browser().close();
+    }
+
+}
